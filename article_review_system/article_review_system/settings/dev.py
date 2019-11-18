c3f36edd525d6a91347357afb490d6538feb9e65
mjs <13394726988@163.com>
2019-11-18 16:19:21 +0800

    模板文件夹创建完毕

--- article_review_system/article_review_system/settings/dev.py
+++ article_review_system/article_review_system/settings/dev.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '5lmx*a^6c-28@1$if!&9c^1rtcr&zgfgz9arggn5wfp=%o_3%h'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,21 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'article_review_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
-        'HOST': '122.114.178.3', # 数据库主机
-        'PORT': 3306, # 数据库端口
-        'USER': 'article_user', # 数据库用户名
-        'PASSWORD': 'Suanyukeji666...+++', # 数据库用户密码
-        'NAME': 'article_review_system' # 数据库名字
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '122.114.178.3',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'article_user',  # 数据库用户名
+        'PASSWORD': 'Suanyukeji666...+++',  # 数据库用户密码
+        'NAME': 'article_review_system'  # 数据库名字
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -119,12 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# 配置静态文件加载路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
