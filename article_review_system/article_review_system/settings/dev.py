64d001961c4701c581da392d5a4ec5d50c049230
mjs <13394726988@163.com>
2019-11-18 15:07:13 +0800

    配置jinja2模板引擎完成

--- article_review_system/article_review_system/settings/dev.py
+++ article_review_system/article_review_system/settings/dev.py
@@ -53,8 +53,8 @@ ROOT_URLCONF = 'article_review_system.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',  # jinja2模板引擎
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            # 补充Jinja2模板引擎环境
+            'environment': 'meiduo_mall.utils.jinja2_env.jinja2_environment',
         },
     },
 ]
